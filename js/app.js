2cdc972dd6dde2b86734a144392c51d3e1b71377
Özge Sıla Erkarslan <ozgeserkarslan@gmail.com>
2020-06-28 20:19:47 +0300

    Login Commit

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 // create login app
 var loginApp = angular.module('loginApp', []);
 
+/*TEST*/
+addUser("ozge","1234567");
+/*******/
 // create login form controller
 loginApp.controller('loginFormController', function($scope) {
 	// function to submit the form after all validation has occurred		
-	$scope.submitForm = function(isValid) {
-		// check to make sure the form is completely valid
+	$scope.submitForm = function(isValid, username, password) {
 		if (isValid) { 
-			alert('Successful!!');
+			// checks if username is in the database
+			if(checkUser(username)){
+				// checks if username and password are correct
+				if(checkPassword(username, password)){
+					alert('Successful!');
+				}
+			}
 		}
 	};
+});
+
+//create register app
+var registerApp = angular.module('registerApp', []);
+
+//create register form controller
+registerApp.controller('registerFormController', function($scope){
+	//function to submit the form after all validation has occurred
+	$scope.submitForm = function(isValid, username, password) {
+		if(isValid){
+			// adds user to database.	
+			db.addUser(username, password);
+			alert('Successful.');
+		}
+	}
 });
--- js/db.js
+++ js/db.js
@@ -11,6 +11,66 @@ db.version(1).stores({
     accounts: "++id,accountName,amount,currency,interestRatio,timeStamp,username",
     transfers: "++id,receiverAccountName,senderAccountName,date,amount,currency,description,username,sentiment"
 });
+
+function addUser(username, password){
+    db.users.add({username: username, password: password}).then(function() {
+        console.log("User added to the users table.");
+    });
+}
+
+function addAccounts(accountName, amount, currency, interestRatio, timeStamp, username){
+    db.accounts.add({accountName: accountName, amount: amount, currency: currency, 
+                    interestRatio: interestRatio, timeStamp: timeStamp, username: username}).then(function(){
+                        console.log("Account added to the accounts table.");
+                    });
+}
+
+function addTransfers(receiverAccountName, senderAccountName, date, amount, currency, description, username, sentiment){
+    db.transfers.add({receiverAccountName: receiverAccountName, senderAccountName: senderAccountName, date: date, 
+                    amount: amount, currency: currency, description: description, username: username, sentiment: sentiment}).then(function(){
+                        console.log("Transfer added to the transfers table.");
+                    });
+}
+
+function checkUser(username){
+    flag = true;
+    db.users.where("username").equals(username).count(function (count) {
+        if(count = 0){
+            alert('Error!');
+            console.log("User is not in the database!");
+            flag = false;
+        }
+        else{
+            flag = true;
+        }
+    });
+    return flag;
+}
+
+function checkPassword(username, password) {
+    flag = true;
+    db.users.where("username").equals(username).each (function (user) {
+        if(password==user.password){
+            flag = true;
+        }
+        else{
+            alert('Error!');
+            console.log("User's password is not matching!");
+            flag = false;
+        }
+    });
+    return flag;
+}
+
+function deleteDatabase(){
+    db.delete().then(() => {
+        console.log("Database successfully deleted");
+    }).catch((err) => {
+        console.error("Could not delete database");
+    }).finally(() => {
+        // Do what should be done next...
+    });
+}
       
 /*
         db.friends.add({name: "Josephine", age: 21}).then(function() {
